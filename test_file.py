bae4ba7409ca07ba9dfc2bb7fcb46b50307c6c22
HunterMcGraw <huntercmcgraw@gmail.com>
2024-12-03 22:56:31 -0500

    Added unit test

--- test_file.py
+++ test_file.py
@@ -1,5 +1,28 @@
 import unittest
-from tetris import check_lines
+from tetris import check_lines, drop_collision_check
+
+class TestDropCollision(unittest.TestCase):
+    def test_no_collision(self):
+        board = [[0 for _ in range(16)] for _ in range(20)]
+        for i, _ in enumerate(board):
+            for j, _ in enumerate(board[i]):
+                if i >= 19 or j <= 1 or j >= 12:
+                    board[i][j] = 1
+
+        board[11][3] = None
+        array = [[500, 150, None], [450, 150, None], [550, 150, None], [550, 100, None]]
+
+        self.assertEqual(drop_collision_check(array, board), False)
+
+    def test_collision(self):
+        board = [[0 for _ in range(16)] for _ in range(20)]
+        for i, _ in enumerate(board):
+            for j, _ in enumerate(board[i]):
+                if i >= 19 or j <= 1 or j >= 12:
+                    board[i][j] = 1
+        array = [[500, 150, None], [450, 150, None], [550, 150, None], [550, 100, None]]
+
+        self.assertEqual(drop_collision_check(array, board), False)
 
 class TestCheckLines(unittest.TestCase):
     def test_no_full_line(self):
